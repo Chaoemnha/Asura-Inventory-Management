6acae5a56a50d4cbc756fc10a0ed1552746258b1
Nguyễn Thiện Luân <137016832+Chaoemnha@users.noreply.github.com>
2025-08-22 15:13:07 +0700

    Khoi tao backend

---
+++ backend/src/test/java/com/inventory/InventoryManagementApplicationTests.java
@@ -0,0 +1,10 @@
+package com.inventory;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class InventoryManagementApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
