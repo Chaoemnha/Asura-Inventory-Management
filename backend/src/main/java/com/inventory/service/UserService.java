99bac33553398d294d14c0908a82e419e2e8e078
Nguyễn Thiện Luân <137016832+Chaoemnha@users.noreply.github.com>
2025-08-22 16:10:19 +0700

    Tao ser, ctrl cho user api

---
+++ backend/src/main/java/com/inventory/service/UserService.java
@@ -0,0 +1,20 @@
+package com.inventory.service;
+
+import com.inventory.dto.LoginRequest;
+import com.inventory.dto.RegisterRequest;
+import com.inventory.dto.Response;
+import com.inventory.dto.UserDTO;
+import com.inventory.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    Response registerUser(RegisterRequest registerRequest);
+    Response loginUser(LoginRequest loginRequest);
+    Response getAllUsers();
+    User getCurrentLoggedInUser();
+    Response updateUser(Long id, UserDTO userDTO);
+    Response deleteUser(Long id);
+    Response getUserTransactions(Long id);
+    List<String> extractTextForEmbedding();
+}
