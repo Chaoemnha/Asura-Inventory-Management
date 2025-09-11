d14f422d98ad9b02c6d3b11147a874e597c289a6
Nguyễn Thiện Luân <137016832+Chaoemnha@users.noreply.github.com>
2025-09-11 23:48:21 +0700

    Giao dien chatbot

---
+++ frontend/src/app/chat-assistant/chat-assistant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-assistant',
+  imports: [],
+  templateUrl: './chat-assistant.component.html',
+  styleUrl: './chat-assistant.component.css', 
+  standalone: true,
+})
+export class ChatAssistantComponent {
+  
+}
